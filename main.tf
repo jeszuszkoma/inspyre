f7e37b9ac61704b35a7ddb76cfacc15e1c60388c
jeszuszkoma <czernamark@gmail.com>
2023-04-07 14:50:57 +0200

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "Terraform-POC"
+    storage_account_name = "terraformpocidentitytest"
+    container_name = "state"
+    key = "terraform.state"
+  }
+}
